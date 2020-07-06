584be5a60c35636c2dec2e4a334c88013a91eb54
haiduk <hwlee91@gmail.com>
2020-07-06 02:34:45 -0400

    small fix

--- sub_strings/sub_string.rb
+++ sub_strings/sub_string.rb
@@ -2,11 +2,12 @@ dictionary = ["below","down","go","going","horn",
 "how","howdy","it","i","low","own","part","partner"
 ,"sit"]
 
-string = "Howdy partner, sit down! How's it going?"
-
 def sub_string(string, dictionary)
 reply=[]
 dictionary.map{|word| reply.push("#{word} =>
 #{string.scan(word).length}")}
 puts reply
 end
+
+sub_string("Howdy partner, sit down! How's it going?",
+dictionary)
