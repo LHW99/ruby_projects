eadb2c953389c357bcce5c27ef2b7e18caa929a7
haiduk <hwlee91@gmail.com>
2020-07-08 05:09:43 -0400

    bubble sort works

--- bubble_sorter/bubble_sorter.rb
+++ bubble_sorter/bubble_sorter.rb
@@ -2,24 +2,15 @@ arr = [5,4,1,3,2]
 
 def bubble_sort(numbers)
   i=0
-  final = numbers.sort
- until numbers == numbers.sort 
+ while numbers != numbers.sort 
   while i<numbers.length - 1
     if numbers[i]>numbers[i+1]
       numbers[i],numbers[i+1] = numbers[i+1],numbers[i]
       i+=1
     else i+=1
     end
+  end
+  i=0
 end
 puts numbers
 end
-
-
-
-=begin
-look at first two numbers in array
-shift higher number to the right by one
-continue down array
-reset and start over
-continue until there are no further changes to be made
-=end
