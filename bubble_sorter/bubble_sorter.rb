706f5d0dbdce91c1c3075504b4be258cc6e71eb9
haiduk <hwlee91@gmail.com>
2020-07-08 04:55:06 -0400

    tinkering with bubble sorter

--- bubble_sorter/bubble_sorter.rb
+++ bubble_sorter/bubble_sorter.rb
@@ -1,6 +1,20 @@
 arr = [5,4,1,3,2]
 
 def bubble_sort(numbers)
+  i=0
+  final = numbers.sort
+ until numbers == numbers.sort 
+  while i<numbers.length - 1
+    if numbers[i]>numbers[i+1]
+      numbers[i],numbers[i+1] = numbers[i+1],numbers[i]
+      i+=1
+    else i+=1
+    end
+end
+puts numbers
+end
+
+
 
 =begin
 look at first two numbers in array
